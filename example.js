ee57928a71f2fa02b505b5a144c34879fca4cb7f
Cesar Brandao <cesarbrandao@radiopopular.pt>
2017-03-17 16:03:30 +0000

    could comment but dont feel like it. Maybe I should tell you a story...

---
+++ example.js
@@ -0,0 +1,4 @@
+// Change './index' to 'is-null-or-empty' if you use this code outside of this package
+var rnd = require('./index');
+
+console.log(rnd.sqr(2,2)); // false
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = function(width, height) {
-  return width * height;
-};
+var Chance = require('chance');
+var fortune = require('fortune-teller');
+var randomWord = require('random-word');
+var nlp = require('compromise');
+module.exports = {
+sqr: function(width, height) {
+	var text =fortune.fortune();
+	var chance = new Chance();
+
+  // Use Chance here.
+  var my_random_string = chance.name();
+    var random = randomWord();
+    r = nlp(text);
+    
+    console.log(r.topics().data());
+	return my_random_string + " " + text + " " + random;
+}
+}
